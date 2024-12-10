0a50f020ee488004cd3b7122f5a0145ff128ef04
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2024-12-09 23:57:05 -0600

    Add player movement animations

--- scene/player.cs
+++ scene/player.cs
@@ -1,13 +1,39 @@
 using Godot;
-using System;
 
 public partial class player : CharacterBody2D
 {
-	public const float Speed = 100.0f;
+    public const float Speed = 100.0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Velocity = Input.GetVector("left", "right", "up", "down") * Speed;
-		MoveAndSlide();
-	}
+    private AnimatedSprite2D _animatedSprite2D;
+
+    public override void _Ready()
+    {
+        _animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Move();
+        AnimateMovement();
+    }
+
+    private void AnimateMovement()
+    {
+        if (Input.IsActionPressed("left"))
+            _animatedSprite2D.Play("left");
+        else if (Input.IsActionPressed("right"))
+            _animatedSprite2D.Play("right");
+        else if (Input.IsActionPressed("up"))
+            _animatedSprite2D.Play("up");
+        else if (Input.IsActionPressed("down"))
+            _animatedSprite2D.Play("down");
+        else
+            _animatedSprite2D.Play("stationary");
+    }
+
+    private void Move()
+    {
+        Velocity = Input.GetVector("left", "right", "up", "down") * Speed;
+        MoveAndSlide();
+    }
 }
