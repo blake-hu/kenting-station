53587aa085638ebd5692587e99dc1404894b71a2
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2025-02-09 18:55:14 -0600

    Set world boundary from editor Rect2 instead

--- Global/WorldBoundary.cs
+++ Global/WorldBoundary.cs
@@ -2,15 +2,23 @@ using Godot;
 
 public partial class WorldBoundary : StaticBody2D
 {
-	public static Rect2 Boundary;
+	public static WorldBoundary Singleton;
+
+	[Export] public Rect2 Boundary;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var xLower = GetNode<CollisionShape2D>("WestBorder").Position.X;
-		var xUpper = GetNode<CollisionShape2D>("EastBorder").Position.X;
-		var yLower = GetNode<CollisionShape2D>("NorthBorder").Position.Y;
-		var yUpper = GetNode<CollisionShape2D>("SouthBorder").Position.Y;
-		Boundary = new Rect2(xLower, yLower, xUpper, yUpper);
+		Singleton = this;
+
+		var west = GetNode<CollisionShape2D>("WestBorder");
+		var east = GetNode<CollisionShape2D>("EastBorder");
+		var north = GetNode<CollisionShape2D>("NorthBorder");
+		var south = GetNode<CollisionShape2D>("SouthBorder");
+
+		west.Position = new Vector2(Boundary.Position.X, 0);
+		east.Position = new Vector2(Boundary.End.X, 0);
+		north.Position = new Vector2(0, Boundary.Position.Y);
+		south.Position = new Vector2(0, Boundary.End.Y);
 	}
 }
