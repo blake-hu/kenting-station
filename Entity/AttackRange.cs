17cafea9fba623149f34fee992aa7685b697268a
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2025-03-07 19:07:29 -0600

    Move namespace of AttackRange

--- Entity/AttackRange.cs
+++ Entity/AttackRange.cs
@@ -5,6 +5,8 @@ using KentingStation.Interface;
 
 // Detect when a predator is within a specified radius of the parent entity
 // and kills the parent entity
+namespace KentingStation.Entity;
+
 public partial class AttackRange : Area2D
 {
     private IKillableEntity _parentEntity;
@@ -15,22 +17,22 @@ public partial class AttackRange : Area2D
     {
         var parentNode = GetParent();
         if (parentNode is not IPredatorPreyEntity predPreyEntity)
-            throw new Exception($"Parent of {nameof(AttackRange)} must be {nameof(IPredatorPreyEntity)}");
+            throw new System.Exception($"Parent of {nameof(AttackRange)} must be {nameof(IPredatorPreyEntity)}");
         _predatorTypes = predPreyEntity.PredatorTypes();
 
         if (parentNode is not IKillableEntity parentEntity)
-            throw new Exception($"Parent of {nameof(AttackRange)} must be {nameof(IKillableEntity)}");
+            throw new System.Exception($"Parent of {nameof(AttackRange)} must be {nameof(IKillableEntity)}");
         _parentEntity = parentEntity;
 
         var collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
         if (collisionShape.Shape is not CircleShape2D circle)
-            throw new Exception(
+            throw new System.Exception(
                 $"{nameof(CollisionShape2D)} must be a {nameof(CircleShape2D)} in {nameof(AttackRange)}");
         circle.Radius = DetectionRadius;
 
         BodyEntered += OnBodyEntered;
     }
-    
+
     private void OnBodyEntered(Node2D body)
     {
         if (body is not CharacterBody2D entity)
