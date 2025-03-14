62133ea35284c8e1537446aec46d5540539879f5
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2025-03-14 05:01:13 -0500

    Continuously plot and save predator-prey population chart

--- Stats/plot_predator_prey.py
+++ Stats/plot_predator_prey.py
@@ -1,6 +1,7 @@
 import re
 import matplotlib.pyplot as plt
 import argparse
+import time
 
 def parse_simulation_output(file_path):
     cow_counts = []
@@ -29,7 +30,7 @@ def parse_simulation_output(file_path):
     return cow_counts, tiger_counts, tree_counts, grass_counts
 
 def plot_predator_prey(cows, tigers, grass):
-    plt.figure(figsize=(10, 5))
+    plt.clf()  # Clear the current figure
     plt.plot(range(len(cows)), cows, label='Cows (Prey)', color='blue')
     plt.plot(range(len(tigers)), tigers, label='Tigers (Predator)', color='red')
     plt.plot(range(len(grass)), grass, label='Grass (Food Source)', color='green')
@@ -38,12 +39,19 @@ def plot_predator_prey(cows, tigers, grass):
     plt.title('Predator-Prey-Resource Population Dynamics')
     plt.legend()
     plt.grid()
-    plt.show()
+    plt.draw()
+    plt.savefig('./plot.png', format='png')
+    plt.pause(3)  # Pause to update the plot every 3 seconds
+
+def continuously_plot(filepath):
+    plt.ion()  # Enable interactive mode
+    while True:
+        cow_population, tiger_population, _, grass_population = parse_simulation_output(filepath)
+        plot_predator_prey(cow_population, tiger_population, grass_population)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse simulation output and plot predator-prey-resource dynamics.")
     parser.add_argument("filepath", type=str, help="Path to the simulation output file")
     args = parser.parse_args()
     
-    cow_population, tiger_population, _, grass_population = parse_simulation_output(args.filepath)
-    plot_predator_prey(cow_population, tiger_population, grass_population)
+    continuously_plot(args.filepath)
